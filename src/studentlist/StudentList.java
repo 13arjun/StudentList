f4941b4bb1df2b90336f96c6604afd173a0dd0d5
test <test@10.48.27.42>
2024-05-13 13:59:05 -0400

    Initial Commit

---
+++ src/studentlist/StudentList.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author test
+ */
+public class StudentList {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Student s1 = new Student();
+        s1.setName("Arjun");
+        
+         Student s2 = new Student();
+        s2.setName("Singh");
+        
+         Student s3 = new Student();
+        s3.setName("Dhillon");
+        
+        Student[] studentList = new Student[3];
+        studentList[0] = s1;
+        studentList[1] = s2;
+        studentList[2] = s3;
+        
+        for (Student s: studentList){
+            System.out.println(s.getName());
+        }
+        
+        
+        
+    }
+    
+}
